8733073a6df6c4d9a6e7f2d192af76823385b0fd
Juan Vasquez <juan.j.vasquez.alfaro@intel.com>
2019-11-12 09:52:12 -0600

    [*] Added distance function

--- kmeans/algorithm.cpp
+++ kmeans/algorithm.cpp
@@ -4,30 +4,48 @@
 
 using namespace std;
 
+float euclideanDistance(vector<float> vector1, vector<float> vector2) {
+	// ||vector1 - vector2|| 
+	vector<float> vectorOut;
+	float squares = 0;
+	for (vector<float>::iterator it = vector1.begin(), jt = vector2.begin(); it != vector1.end(); ++it) {
+		float component = *it - *jt;
+		vectorOut.push_back(component);
+		printf("%f - %f = %f\n", *it, *jt, component);
+		++jt;
+		squares += component * component;
+	}
+	
+	return sqrt(squares);
+}	
+
+void updateCentroid(vector<float> data[], vector<float> centroid) {
+	
+}
+
 void cluster(vector<float> data[], int clusters) {
 	//	Select first element of data
 	//	Define initial centroids: 
-	//vector<double> centroids[clusters - 1] = [ data[first], data[middle], data[last] ];
-	
+	vector<float> *centroids;
+	centroids = new vector<float>[clusters];
+
+	for (int i = 0; i < clusters; i++) {
+		centroids[i] = data[i];
+	}
+
 	//	Iterate over array data[] foreach data[i]:
-	for (int i = 0; i <= data->size() - 1; i++) {
+	for (int i = 0; i < data->size() - 1; i++) {
 		printf("Value of vector = ");
-		//	Iterate over vector values
-		for(vector<float>::iterator it = data[i].begin(); it != data[i].end(); ++it) {
+		//	Iterate over vector's elements
+		for (vector<float>::iterator it = data[i].begin(); it != data[i].end(); ++it) {
 			printf("%f ", *it);
 		}
 		printf("\n");
 
-		
-
+		printf("Distancia = %f \n", euclideanDistance(data[i], centroids[1]));
 	}
-		
-}
 
-void euclideanDistance(vector<float> vector1, vector<float> vector2) {
-	// ||vector1 - vector2|| 
-}	
+	delete [] centroids;
 
-void updateCentroid(vector<float> data[], vector<double> centroid) {
-	
+
 }
--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -8,23 +8,22 @@ const int N = 2; //Number of data pointsr
 
 int main()
 {
-	//TODO Define data input
-
-	//callKmeans function on data input
+	// Define data input
 	vector<float> data[N];
 	vector<float> A, B;
 	A.push_back(51.52f);
-	A.push_back(140.21f);
+	A.push_back(-140.21f);
+	A.push_back(10.21f);
 	B.push_back(84.502f);
 	B.push_back(0.0512f);
+	B.push_back(-5.15f);
 	data[0] = A;
 	data[1] = B;
 
-
-	std::cout << "Array = 0x" << data << ", data: " << *data[0].data() << endl;
 	std::cout << "WIP!\n";
 
-	cluster(data, 5);
+	//callKmeans function on data input
+	cluster(data, 2);
 	
 	//output result
 }
